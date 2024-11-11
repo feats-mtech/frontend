f106d135b1ea29a0968df44840953974708fbafa
pinardy <pinardy.yang@gmail.com>
2024-11-11 17:10:08 +0800

    test: add test for filter ingredient

--- __tests__/ingredient/Ingredient.test.tsx
+++ __tests__/ingredient/Ingredient.test.tsx
@@ -1,5 +1,7 @@
 import '@testing-library/jest-dom';
 import React from 'react';
+import userEvent from '@testing-library/user-event';
+
 import * as useAuth from '../../src/context/AuthContext';
 import * as useRouter from '../../src/routes/hooks/use-router';
 
@@ -47,11 +49,36 @@ describe('InventoryView', () => {
 
     render(<InventoryView />);
 
-    // expect to have the ingredients fetched from API call to be displayed
+    // display ingredients fetched from API call in table
     await waitFor(() => {
       expect(screen.getByText(/apple/i)).toBeInTheDocument();
       expect(screen.getByText(/orange/i)).toBeInTheDocument();
       expect(screen.getByText(/papaya/i)).toBeInTheDocument();
+    });
+  });
+
+  it('filters ingredients by name in search bar', async () => {
+    jest
+      .spyOn(require('../../src/dao/ingredientDao'), 'getIngredientsByUser')
+      .mockResolvedValue(mockIngredients);
+
+    render(<InventoryView />);
+
+    const input = screen.getByPlaceholderText(/Search/i);
+
+    await userEvent.click(input);
+    await userEvent.type(input, 'apple');
+
+    await waitFor(() => {
+      expect(screen.getByText(/apple/i)).toBeInTheDocument();
+      expect(screen.queryByText(/orange/i)).not.toBeInTheDocument();
+      expect(screen.queryByText(/papaya/i)).not.toBeInTheDocument();
+    });
+
+    await userEvent.type(input, 'mango'); // mango does not exist
+    await waitFor(() => {
+      expect(screen.getByText('Not found')).toBeInTheDocument();
+      expect(screen.getByText(/No results found for/i)).toBeInTheDocument;
     });
   });
 });
