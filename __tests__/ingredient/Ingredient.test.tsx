16ecefd3e218d1f0ad95ce225a0b47a23ccc126c
Pinardy <pinardy.yang@gmail.com>
2024-11-11 17:11:34 +0800

    test: add ingredient unit tests (#67)
    * test: add ingredient test to display ingredients
    
    * test: add test for filter ingredient
    
    * chore: remove unused test

--- __tests__/ingredient/Ingredient.test.tsx
+++ __tests__/ingredient/Ingredient.test.tsx
@@ -1,10 +1,13 @@
+import '@testing-library/jest-dom';
 import React from 'react';
+import userEvent from '@testing-library/user-event';
+
 import * as useAuth from '../../src/context/AuthContext';
 import * as useRouter from '../../src/routes/hooks/use-router';
 
 import { render, screen, waitFor } from '@testing-library/react';
 import { InventoryView } from '../../src/sections/inventory/view/inventory-view';
-import '@testing-library/jest-dom';
+import { mockIngredients } from '../utils/mockIngredients';
 
 describe('InventoryView', () => {
   // mock contexts and router
@@ -35,6 +38,47 @@ describe('InventoryView', () => {
     render(<InventoryView />);
     await waitFor(() => {
       expect(screen.getByText('Inventory')).toBeInTheDocument();
+    });
+  });
+
+  it('displays user ingredients in a table', async () => {
+    // Mock the getIngredientsByUser function to return mockIngredients
+    jest
+      .spyOn(require('../../src/dao/ingredientDao'), 'getIngredientsByUser')
+      .mockResolvedValue(mockIngredients);
+
+    render(<InventoryView />);
+
+    // display ingredients fetched from API call in table
+    await waitFor(() => {
+      expect(screen.getByText(/apple/i)).toBeInTheDocument();
+      expect(screen.getByText(/orange/i)).toBeInTheDocument();
+      expect(screen.getByText(/papaya/i)).toBeInTheDocument();
+    });
+  });
+
+  it('filters ingredients by name in search bar', async () => {
+    jest
+      .spyOn(require('../../src/dao/ingredientDao'), 'getIngredientsByUser')
+      .mockResolvedValue(mockIngredients);
+
+    render(<InventoryView />);
+
+    const input = screen.getByPlaceholderText(/Search/i);
+
+    await userEvent.click(input);
+    await userEvent.type(input, 'apple');
+
+    await waitFor(() => {
+      expect(screen.getByText(/apple/i)).toBeInTheDocument();
+      expect(screen.queryByText(/orange/i)).not.toBeInTheDocument();
+      expect(screen.queryByText(/papaya/i)).not.toBeInTheDocument();
+    });
+
+    await userEvent.type(input, 'mango'); // mango does not exist
+    await waitFor(() => {
+      expect(screen.getByText('Not found')).toBeInTheDocument();
+      expect(screen.getByText(/No results found for/i)).toBeInTheDocument;
     });
   });
 });
