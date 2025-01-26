08583869c158107cefb3fbd2c20a194551f38d54
pinardy <pinardy.yang@gmail.com>
2025-01-26 18:58:40 +0800

    fix: conditional https config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,22 @@ import path from 'path';
 import checker from 'vite-plugin-checker';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import fs from 'fs';
 
 export default ({ mode }: { mode: string }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   const envPrefix = process.env.NODE_ENV;
+
+  const keyPath = '/etc/ssl/certs/key.pem';
+  const certPath = '/etc/ssl/certs/cert.pem';
+
+  const httpsConfig =
+    fs.existsSync(keyPath) && fs.existsSync(certPath)
+      ? {
+          key: fs.readFileSync(keyPath),
+          cert: fs.readFileSync(certPath),
+        }
+      : false;
 
   return defineConfig({
     base: './',
@@ -44,10 +56,11 @@ export default ({ mode }: { mode: string }) => {
     server: {
       port: Number(process.env.VITE_PORT),
       host: process.env.VITE_HOST,
-      https: {
-        key: fs.readFileSync('/etc/ssl/certs/key.pem'),
-        cert: fs.readFileSync('/etc/ssl/certs/cert.pem'),
-      },
+      https: httpsConfig,
+      // https: {
+      //   key: fs.readFileSync('/etc/ssl/certs/key.pem'),
+      //   cert: fs.readFileSync('/etc/ssl/certs/cert.pem'),
+      // },
     },
     preview: { port: Number(process.env.VITE_PORT), host: true },
   });
