b85cb4ad61c25fa7ddd97aa1c3c74fc7bccc1cff
Pinardy <pinardy.yang@gmail.com>
2025-03-07 08:55:15 +0800

    chore: remove unused then statements (#109)

--- src/dao/ingredientDao.ts
+++ src/dao/ingredientDao.ts
@@ -47,16 +47,14 @@ export const getIngredientsByUser = async (userId: number): Promise<Ingredient[]
 
 export const updateIngredient = async (ingredientDetails: IngredientRowProps, userId: number) => {
   try {
-    const result = await axiosInstance
-      .post(`${backendUrl}/ingredient/update`, {
-        id: ingredientDetails.id,
-        name: ingredientDetails.name,
-        userId,
-        quantity: ingredientDetails.quantity,
-        uom: ingredientDetails.uom,
-        expiryDate: ingredientDetails.expiryDate,
-      })
-      .then((response) => response);
+    const result = await axiosInstance.post(`${backendUrl}/ingredient/update`, {
+      id: ingredientDetails.id,
+      name: ingredientDetails.name,
+      userId,
+      quantity: ingredientDetails.quantity,
+      uom: ingredientDetails.uom,
+      expiryDate: ingredientDetails.expiryDate,
+    });
     return { success: result.status === HttpStatusCode.Ok };
   } catch (error) {
     return { success: false, error: error.message };
@@ -65,9 +63,7 @@ export const updateIngredient = async (ingredientDetails: IngredientRowProps, us
 
 export const deleteIngredient = async (ingredientId: number) => {
   try {
-    const result = await axiosInstance
-      .delete(`${backendUrl}/ingredient/delete/${ingredientId}`)
-      .then((response) => response);
+    const result = await axiosInstance.delete(`${backendUrl}/ingredient/delete/${ingredientId}`);
     return { success: result.status === HttpStatusCode.Ok };
   } catch (error) {
     return { success: false, error: error.message };
