c5ab3395be1982317ef57c252c1b572f44e81fd8
Pinardy <pinardy.yang@gmail.com>
2025-03-10 13:25:14 +0800

    ci: fix pipeline repo names (#112)

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker pull zaproxy/zap-stable
-docker run -i zaproxy/zap-stable zap-baseline.py -t "https://github.com/dmss-group3-practice-module/frontend/" -l PASS > zap_baseline_report.html
+docker run -i zaproxy/zap-stable zap-baseline.py -t "https://github.com/sus-practice-module/frontend/" -l PASS > zap_baseline_report.html
 
 echo $? > /dev/null
