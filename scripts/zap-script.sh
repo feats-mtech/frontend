0592b72f3116dd7116db131fd58a64114ed058c5
pinardy <pinardy.yang@gmail.com>
2025-03-23 10:30:18 +0800

    ci: update repo path

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker pull zaproxy/zap-stable
-docker run -i zaproxy/zap-stable zap-baseline.py -t "https://github.com/feats/frontend/" -l PASS > zap_baseline_report.html
+docker run -i zaproxy/zap-stable zap-baseline.py -t "https://github.com/feats-mtech/frontend/" -l PASS > zap_baseline_report.html
 
 echo $? > /dev/null
