0d3743de4eadf2a7fec0ff36ca39793c0af34265
Pinardy <pinardy.yang@gmail.com>
2025-09-04 17:53:45 +0800

    refactor: otel with web-vitals

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,23 @@ import { Router } from 'src/routes/sections';
 import { useScrollToTop } from 'src/hooks/use-scroll-to-top';
 import { ThemeProvider } from 'src/theme/theme-provider';
 import { AuthProvider } from './context/AuthContext';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { onCLS, onFCP, onLCP, onTTFB, onINP } from 'web-vitals';
+import { reportWebVitals } from './otel-init';
 
 export default function App() {
+  const location = useLocation();
+
+  // Send each web-vitals metric as a Jaeger span
+  useEffect(() => {
+    onCLS(reportWebVitals);
+    onFCP(reportWebVitals);
+    onLCP(reportWebVitals);
+    onTTFB(reportWebVitals);
+    onINP(reportWebVitals);
+  }, [location]);
+
   useScrollToTop();
 
   return (
--- src/otel-init.ts
+++ src/otel-init.ts
@@ -3,6 +3,7 @@ import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { trace } from '@opentelemetry/api';
 import { resourceFromAttributes } from '@opentelemetry/resources';
+import { CLSMetric, FCPMetric, LCPMetric, TTFBMetric, INPMetric } from 'web-vitals';
 
 const provider = new WebTracerProvider({
   resource: resourceFromAttributes({
@@ -16,5 +17,25 @@ const otlpExporter = new OTLPTraceExporter({
 
 (provider as any)._activeSpanProcessor = new SimpleSpanProcessor(otlpExporter);
 provider.register();
+
+export const reportWebVitals = (
+  metric: CLSMetric | FCPMetric | LCPMetric | TTFBMetric | INPMetric,
+) => {
+  const span = tracer.startSpan(`WebVital:${metric.name}`);
+  span.setAttribute('value', metric.value);
+  span.setAttribute('rating', metric.rating);
+  span.setAttribute('delta', metric.delta);
+  span.setAttribute('id', metric.id);
+
+  // console.log(`${metric.name}: ${metric.value}, ${metric.rating}`);
+  metric.entries?.forEach((entry, index) => {
+    span.setAttribute(`entry[${index}].startTime`, entry.startTime);
+    span.setAttribute(`entry[${index}].duration`, entry.duration);
+  });
+  span.end();
+  if (metric.entries) {
+    span.setAttribute('entries.length', metric.entries.length);
+  }
+};
 
 export const tracer = trace.getTracer('web-vitals-tracer');
