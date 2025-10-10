ce4c4dfe405608542108601f8af419d606454df6
Pinardy <pinardy.yang@gmail.com>
2025-10-10 11:06:04 +0800

    feat: add elastic-apm for instrumentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
+import './apm-init'; // Initializes Elastic APM
 import 'src/global.css';
 
 import { Router } from 'src/routes/sections';
 import { useScrollToTop } from 'src/hooks/use-scroll-to-top';
 import { ThemeProvider } from 'src/theme/theme-provider';
 import { AuthProvider } from './context/AuthContext';
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { onCLS, onFCP, onLCP, onTTFB, onINP } from 'web-vitals';
-import { reportWebVitals } from './otel-init';
+// import { useEffect } from 'react';
+// import { useLocation } from 'react-router-dom';
+// import { onCLS, onFCP, onLCP, onTTFB, onINP } from 'web-vitals';
+// import { reportWebVitals } from './otel-init';
 
 export default function App() {
-  const location = useLocation();
+  // const location = useLocation();
 
   // Send each web-vitals metric as a Jaeger span
-  useEffect(() => {
-    onCLS(reportWebVitals);
-    onFCP(reportWebVitals);
-    onLCP(reportWebVitals);
-    onTTFB(reportWebVitals);
-    onINP(reportWebVitals);
-  }, [location]);
+  // useEffect(() => {
+  //   onCLS(reportWebVitals);
+  //   onFCP(reportWebVitals);
+  //   onLCP(reportWebVitals);
+  //   onTTFB(reportWebVitals);
+  //   onINP(reportWebVitals);
+  // }, [location]);
 
   useScrollToTop();
 
